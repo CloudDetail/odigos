6d11ede83d286d8b0350d03322a2bbad1ddefa0b
yunguannjg <18757593931@163.com>
2024-07-29 15:08:27 +0800

    feat: 默认添加application_name

--- odiglet/pkg/env/custom_endpoint.go
+++ odiglet/pkg/env/custom_endpoint.go
@@ -2,18 +2,22 @@ package env
 
 import (
 	"os"
+	"strings"
 )
 
 const (
 	OTEL_EXPORTER_OTLP_GRPC_ENDPOINT = "OTEL_EXPORTER_OTLP_GRPC_ENDPOINT"
 	OTEL_EXPORTER_OTLP_HTTP_ENDPOINT = "OTEL_EXPORTER_OTLP_HTTP_ENDPOINT"
 	OTEL_EXPORTER_OPAMP_ENDPOINT     = "OTEL_EXPORTER_OPAMP_ENDPOINT"
+	OTEL_AUTO_SERVICE_NAME           = "OTEL_AUTO_SERVICE_NAME"
 )
 
 type CustomOtlpENDPOINT struct {
 	OtlpHTTPEndpoint string
 	OtlpGrpcEndpoint string
 	OpAMPEndpoint    string
+
+	AutoServiceName bool
 }
 
 func LoadCustomEndpoint() CustomOtlpENDPOINT {
@@ -32,9 +36,16 @@ func LoadCustomEndpoint() CustomOtlpENDPOINT {
 		opampEndpoint = ""
 	}
 
+	autoServiceOption := true
+	autoServiceName, ok := os.LookupEnv(OTEL_AUTO_SERVICE_NAME)
+	if !ok || strings.ToLower(autoServiceName) != "true" {
+		autoServiceOption = false
+	}
+
 	return CustomOtlpENDPOINT{
 		OtlpHTTPEndpoint: httpEndpoint,
 		OtlpGrpcEndpoint: grpcEndpoint,
 		OpAMPEndpoint:    opampEndpoint,
+		AutoServiceName:  autoServiceOption,
 	}
 }
