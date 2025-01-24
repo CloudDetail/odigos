07b53ccd9892105fc254ab1353b5046b07818c46
yunguannjg <18757593931@163.com>
2025-01-24 10:05:35 +0800

    fix: update instruments with hash check

--- odiglet/pkg/instrumentation/fs/agents.go
+++ odiglet/pkg/instrumentation/fs/agents.go
@@ -1,10 +1,13 @@
 package fs
 
 import (
+	"crypto/sha256"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 	"syscall"
 
 	"github.com/odigos-io/odigos/odiglet/pkg/log"
@@ -19,29 +22,38 @@ const (
 )
 
 func CopyAgentsDirectoryToHost() error {
-
 	// remove the current content of /var/odigos
 	// as we want a fresh copy of instrumentation agents with no files leftover from previous odigos versions.
 	// we cannot remove /var/odigos itself: "unlinkat /var/odigos: device or resource busy"
 	// so we will just remove it's content
-	entries, err := os.ReadDir(hostDir)
+
+	// We kept the following list of files to avoid removing instrumentations that are already loaded in the process memory
+	filesToKeep := map[string]struct{}{
+		"/var/odigos/nodejs-ebpf/build/Release/dtrace-injector-native.node":                            {},
+		"/var/odigos/nodejs-ebpf/build/Release/obj.target/dtrace-injector-native.node":                 {},
+		"/var/odigos/nodejs-ebpf/build/Release/.deps/Release/dtrace-injector-native.node.d":            {},
+		"/var/odigos/nodejs-ebpf/build/Release/.deps/Release/obj.target/dtrace-injector-native.node.d": {},
+		"/var/odigos/java-ebpf/tracing_probes.so":                                                      {},
+		"/var/odigos/java-ext-ebpf/end_span_usdt.so":                                                   {},
+		"/var/odigos/python-ebpf/pythonUSDT.abi3.so":                                                   {},
+	}
+
+	updatedFilesToKeepMap, err := removeChangedFilesFromKeepMap(filesToKeep, containerDir, hostDir)
 	if err != nil {
+		log.Logger.Error(err, "Error getting changed files")
+	}
+
+	err = removeFilesInDir(hostDir, updatedFilesToKeepMap)
+	if err != nil {
+		log.Logger.Error(err, "Error removing instrumentation directory from host")
 		return err
 	}
-	for _, entry := range entries {
-		entryPath := filepath.Join(hostDir, entry.Name())
-		err := os.RemoveAll(entryPath)
-		if err != nil {
-			return err
-		}
-	}
 
-	cmd := exec.Command("/bin/bash", "-c", "cp -r " + containerDir + "/* "  + hostDir)
-	output, err := cmd.CombinedOutput()
+	err = copyDirectories(containerDir, hostDir, updatedFilesToKeepMap)
 	if err != nil {
-		return fmt.Errorf("error copying agents directory to host: %v, output %s", err, output)
+		log.Logger.Error(err, "Error copying instrumentation directory to host")
+		return err
 	}
-
 
 	// Check if the semanage command exists when running on RHEL/CoreOS
 	_, err = exec.LookPath(filepath.Join(chrootDir, semanagePath))
@@ -68,4 +80,89 @@ func CopyAgentsDirectoryToHost() error {
 	}
 
 	return nil
+}
+
+func removeChangedFilesFromKeepMap(filesToKeepMap map[string]struct{}, containerDir string, hostDir string) (map[string]struct{}, error) {
+
+	updatedFilesToKeepMap := make(map[string]struct{})
+
+	for hostPath := range filesToKeepMap {
+		// Convert host path to container path
+		containerPath := strings.Replace(hostPath, hostDir, containerDir, 1)
+
+		// If either file doesn't exist, mark as changed and remove from filesToKeepMap
+		_, hostErr := os.Stat(hostPath)
+		_, containerErr := os.Stat(containerPath)
+
+		if hostErr != nil || containerErr != nil {
+			log.Logger.V(0).Info("File marked for recreate (missing)", "file", hostPath)
+			continue
+		}
+
+		// Compare file hashes
+		hostHash, err := calculateFileHash(hostPath)
+		if err != nil {
+			return nil, fmt.Errorf("error calculating hash for host file %s: %v", hostPath, err)
+		}
+
+		containerHash, err := calculateFileHash(containerPath)
+		if err != nil {
+			return nil, fmt.Errorf("error calculating hash for container file %s: %v", containerPath, err)
+		}
+
+		// If the hashes are different, keep the old version of the file in the host with the new name <ORIGINAL_FILE_NAME_{12_CHARS_OF_HASH}>
+		// and ensure the renamed file is added to filesToKeepMap to protect it from deletion.
+		if hostHash != containerHash {
+			newHostPath, err := renameFileWithHashSuffix(hostPath, hostHash)
+			if err != nil {
+				return nil, fmt.Errorf("error renaming file: %v", err)
+			}
+
+			updatedFilesToKeepMap[newHostPath] = struct{}{}
+
+			continue // original file is renamed, recreate hostPath and keep NewHostPath
+		}
+
+		updatedFilesToKeepMap[hostPath] = struct{}{}
+	}
+
+	return updatedFilesToKeepMap, nil
+}
+
+// Helper function to rename a file using the first 12 characters of its hash
+func renameFileWithHashSuffix(originalPath, fileHash string) (string, error) {
+	// Extract the first 12 characters of the hash
+	hashSuffix := fileHash[:12]
+
+	newPath := generateRenamedFilePath(originalPath, hashSuffix)
+
+	if err := os.Rename(originalPath, newPath); err != nil {
+		return "", fmt.Errorf("failed to rename file %s to %s: %w", originalPath, newPath, err)
+	}
+
+	log.Logger.V(0).Info("File successfully renamed", "oldPath", originalPath, "newPath", newPath)
+	return newPath, nil
+}
+
+// Construct a renamed file path
+func generateRenamedFilePath(originalPath, hashSuffix string) string {
+	ext := filepath.Ext(originalPath)                                 // Get the file extension (e.g., ".so")
+	base := strings.TrimSuffix(originalPath, ext)                     // Remove the extension from the original path
+	return fmt.Sprintf("%s_hash_version-%s%s", base, hashSuffix, ext) // Append the hash and add back the extension
+}
+
+// calculateFileHash computes the SHA-256 hash of a file
+func calculateFileHash(filePath string) (string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	hasher := sha256.New()
+	if _, err := io.Copy(hasher, file); err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%x", hasher.Sum(nil)), nil
 }
