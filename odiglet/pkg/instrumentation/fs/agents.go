7ce903fc6cda2fb1ddd307bb0f21ecf93acaa088
yunguannjg <18757593931@163.com>
2025-02-24 12:01:35 +0800

    fix: keep the original javaagent's jar if is not updated

--- odiglet/pkg/instrumentation/fs/agents.go
+++ odiglet/pkg/instrumentation/fs/agents.go
@@ -36,6 +36,8 @@ func CopyAgentsDirectoryToHost() error {
 		"/var/odigos/java-ebpf/tracing_probes.so":                                                      {},
 		"/var/odigos/java-ext-ebpf/end_span_usdt.so":                                                   {},
 		"/var/odigos/python-ebpf/pythonUSDT.abi3.so":                                                   {},
+		"/var/odigos/skywalking/java/skywalking-agent.jar":                                             {},
+		"/var/odigos/java/javaagent.jar":                                                               {},
 	}
 
 	updatedFilesToKeepMap, err := removeChangedFilesFromKeepMap(filesToKeep, containerDir, hostDir)
